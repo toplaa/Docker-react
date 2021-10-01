b373c08cf25ee57e0df2133f48800449f59ab580
Temitope <temitope.oladele@gmail.com>
2021-09-30 22:58:57 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,13 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  
 });
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  
+});
+
